461c7632afd8d98b657cfd693469672568cdd6d8
amrhosseinghasemii <a.h.ghasemi1199@gmail.com>
2024-08-11 06:40:30 +0330

    splash screen added

--- wordle_game/lib/main.dart
+++ wordle_game/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wordle_game/pages/home_page.dart';
+import 'package:wordle_game/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +16,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          primary: Color.fromRGBO(244, 121, 143, 1),
+          primaryFixed: Color.fromRGBO(64, 75, 93, 1),
+          primaryFixedDim: Color.fromRGBO(124, 201, 184, 1),
+          ),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: splashPage(),
     );
   }
 }
